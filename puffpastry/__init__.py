33833da92bb142daab9b5de856e154722c9ed3e0
Adam Ever-Hadani <adamhadani@gmail.com>
2024-07-04 13:28:23 -0500

    skeleton framework

---
+++ puffpastry/__init__.py
@@ -0,0 +1,88 @@
+from functools import wraps
+from os import getenv
+
+import dotenv
+
+
+_APP = None
+_REGISTRY = dict()
+_CACHE = dict()
+
+
+class DotEnvConfig:
+    """
+    Default configuration provider. Uses dotenv which allows for configuration via
+    environment variables and/or .env files.
+    
+    """
+    def __init__(self):
+        dotenv.load_dotenv()
+
+    def __getattr__(self, name):
+        return getenv(name.upper())
+
+
+class Application:
+    """
+    High-level application object provides access to the dependency graph and metadata.
+    
+    """
+    def __init__(self, identifier, config=None):
+        global _CACHE, _REGISTRY
+
+        if config is None:
+            config = DotEnvConfig()
+
+        self.identifier = identifier
+        self.registry = _REGISTRY
+        self.cache = _CACHE
+        self.config = config
+
+    def __getattr__(self, name):
+        if name in self.cache:
+            return self.cache[name]
+        elif name in self.registry:
+            self.cache[name] = self.registry[name](self)
+            return self.cache[name]
+        else:
+            raise AttributeError(f"{name} is not a registered component.")
+    
+    def load(self, *identifiers):
+        for identifier in identifiers:
+            getattr(self, identifier)
+        
+        return self
+
+
+
+def create(identifier: str):
+    """Create an application namespace associated with given identifier."""
+    global _APP
+    if _APP is not None:
+        raise KeyError(f"Application object already initialized.")
+
+    _APP = Application(identifier)
+
+    return _APP   
+
+
+def provides(identifier: str):
+    """
+    Register a dependency in the global app registry associated with given identifier.
+    
+    """
+    global _REGISTRY
+
+    if identifier in _REGISTRY:
+        raise KeyError(f"identifier {identifier} is already assigned an object in the application.")
+    
+    def decorated(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        
+        _REGISTRY[identifier] = wrapper
+    
+        return wrapper
+        
+    return decorated
